9948af22c06b4a168a869f2416558e51912725e3
lihua <mrlu0208@gmail.com>
2023-04-17 19:33:24 -0600

    first commit

---
+++ src/main/java/com/comit/users/ComitUsersSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.comit.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComitUsersSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComitUsersSimpleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comit/users/ComitUsersSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comit.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComitUsersSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
